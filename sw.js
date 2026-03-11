27ee6d41c93ba2408828cde7ad290e772613bf5e
Bravo6actual <bravovector6@gmail.com>
2026-03-11 07:00:27 -0700

    Add WLF logo as PWA app icon (192px + 512px)

--- sw.js
+++ sw.js
@@ -9,6 +9,8 @@ const ASSETS = [
   './index.html',
   './wlf-logo.jpg',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   // Google Fonts (cached on first load)
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Mono:wght@400;500&family=DM+Sans:wght@300;400;500&display=swap',
 ];
